75ff09eb56542c260649082351c8b270ac1faddb
zhangxiang <yyy67239667@163.com>
2020-10-09 18:39:33 +0800

    提交

---
+++ src/settings.js
@@ -0,0 +1,4 @@
+// 默认配置
+module.exports = {
+  title: 'app'
+}
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
+'use strict'
+const defaultSettings = require('./src/settings.js')
 
+const name = defaultSettings.title || 'vue Admin Template' // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following methods:
+// port = 9528 npm run dev OR npm run dev --port = 9528
 const port = process.env.port || process.env.npm_config_port || 8080 // dev port
+
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
 
 module.exports = {
   publicPath: './',
@@ -23,5 +34,18 @@ module.exports = {
         }
       }
     }
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name
+  },
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = name
+        return args
+      })
   }
 }
