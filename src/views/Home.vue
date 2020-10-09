b79da473e933cd5e33845ff3a416c9dacec2aa3a
zhangxiang <yyy67239667@163.com>
2020-10-09 16:56:48 +0800

    引入vant

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">引入vant</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+import { Button } from 'vant'
+Vue.use(Button)
 
 export default {
   name: 'Home',
