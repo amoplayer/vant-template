734df937d8e4801ba62614a04323bf0d0578ed34
zhangxiang <yyy67239667@163.com>
2020-10-10 09:18:57 +0800

    修改文件错误名称

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,25 +1,19 @@
 <template>
   <div>
     <app-main />
-    <Fouter></Fouter>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import AppMain from './components/AppMain'
-import Fouter from './components/Fouter'
-
-import Vue from 'vue'
-import { Tabbar, TabbarItem } from 'vant'
-
-Vue.use(Tabbar)
-Vue.use(TabbarItem)
+import Footer from './components/Footer'
 
 export default {
   name: 'Layout',
   components: {
     AppMain,
-    Fouter
+    Footer
   },
   created() {
     console.log(process.env.VUE_APP_BASE_API)
